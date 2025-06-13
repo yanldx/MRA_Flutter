5f7ad635cff5e8ecadb317650f2b8baf73830269
Sorais <soraisv2@gmail.com>
2025-06-13 16:39:07 +0200

    Dynamic Rover home page - data of selected orver + loader

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,13 +5,9 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Mars Rover API'),
-      ),
+      appBar: AppBar(title: Text('Mars Rover API')),
       drawer: AppDrawer(),
-      body: Center(
-        child: Text('Bienvenue sur l\'application Mars Rover'),
-      ),
+      body: Center(child: Text('Bienvenue sur l\'application Mars Rover')),
     );
   }
 }
